b417cec2b0bbe9b4fdc0f6f2aafe732c6dff0a40
Sid Sun <sid@sidsun.com>
2022-10-19 17:45:16 +0530

    Fix v2 fetch & refactor crypto
    the output of GCM Encypt contains 16 bits of authentication / AEAD data which breaks JSON unmarhal on v2
    the bug wa supressed on v1 due to zlib compression ignoring the data
    also, update some comments and remove unused cfb encrypt function
    
    Signed-off-by: Sid Sun <sid@sidsun.com>

--- src/utils/crypto.go
+++ src/utils/crypto.go
@@ -37,13 +37,10 @@ func GenKey(key, salt []byte) []byte {
 }
 
 func Encrypt(data []byte, key []byte, salt *[32]byte) (ciphertext []byte, iv []byte, keysalt []byte) {
-	// Generate cipher
 	c, _ := aes.NewCipher(key)
 
-	// use CFB to encrypt full data
 	ciphertext, iv = gcmEncrypt(data, c)
 
-	// prepend salt to the data
 	return ciphertext, iv, (*salt)[:]
 }
 
@@ -54,28 +51,11 @@ func Decrypt(data []byte, salt []byte, iv []byte, key []byte) []byte {
 	// Generate cipher
 	c, _ := aes.NewCipher(key)
 
-	// Send IV and data bits to decrypt via CFB
 	// returned data does not have salt
 	return gcmDecrypt(data, iv, c)
 }
 
-func cfbEncrypt(data []byte, blockCipher cipher.Block) []byte {
-	// Create dst with length of cipher blocksize + data length
-	// And initialize first BlockSize bytes pseudorandom for IV
-	dst := make([]byte, blockCipher.BlockSize()+len(data))
-
-	// Read random values from crypto/rand for CFB initialization vector
-	// Error can be safely ignored
-	io.ReadFull(rand.Reader, dst[:blockCipher.BlockSize()])
-
-	// dst from 0 to blockSize is the IV
-	cfb := cipher.NewCFBEncrypter(blockCipher, dst[:blockCipher.BlockSize()])
-	cfb.XORKeyStream(dst[blockCipher.BlockSize():], data)
-	return dst
-}
-
 func gcmDecrypt(data []byte, nonce []byte, blockCipher cipher.Block) []byte {
-	// Create CFB Decrypter with cipher, instantiating with IV (first blockSize blocks of data)
 	gcm, err := cipher.NewGCMWithNonceSize(blockCipher, 32)
 	if err != nil {
 		panic(err)
@@ -86,7 +66,9 @@ func gcmDecrypt(data []byte, nonce []byte, blockCipher cipher.Block) []byte {
 	if err != nil {
 		panic(err)
 	}
-	return data
+
+	// Remove 16 bits of authentication data from the end
+	return data[:len(data)-16]
 }
 
 func gcmEncrypt(data []byte, blockCipher cipher.Block) (ciphertext []byte, nonce []byte) {
@@ -106,6 +88,6 @@ func gcmEncrypt(data []byte, blockCipher cipher.Block) (ciphertext []byte, nonce
 		panic(err)
 	}
 
-	// prepend generated nonce to data
+	// Output contains 16 bits of authentication data
 	return ciphertext, nonce
 }
